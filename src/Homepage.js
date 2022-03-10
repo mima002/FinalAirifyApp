4c01b74444b8b77711182c4851e60131bc760675
mima002 <mima002@uw.edu>
2022-03-09 22:19:13 -0800

    deploy with firebase

--- src/Homepage.js
+++ src/Homepage.js
@@ -3,16 +3,35 @@ import InputWithIcon from "./SearchBar";
 import CityAirQuality from "./CityAirQuality";
 import BottomBar from "./BottomBar";
 import { Link } from "react-router-dom";
+import { useState } from 'react';
 
 // homepage function
 export default function Homepage() {
+  const [text, setText] = useState(""); 
+  const [city, setCity] = useState([]);
+
+
+  async function search() {
+    const key = "27501b420146429e3169f8e098265b233cf0450c";
+    const url = `https://api.waqi.info/feed/${text}/?token=${key}`;
+    const r = await fetch(url);
+    const j = await r.json();
+    setCity(j.data.city.name);
+  }
+
   return (
     <div className="App">
       {/* header with title and search bar */}
       <header className="header">
         <div className="title">Airify</div>
         <div className="search-city">
-          <InputWithIcon />
+          <InputWithIcon 
+          onChange={(e) => setText(e.target.value)}
+          value={text}
+          onKeyPress={(e) => {
+            if (e.key === "Enter") search();
+          }}
+          />
         </div>
       </header>
 
